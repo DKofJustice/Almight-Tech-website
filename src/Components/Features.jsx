c9977a44356c8cf82ad2184164c6c7cd2317fa1b
Alex Benta <alexbenta20@gmail.com>
2022-12-09 23:11:25 +0200

    Styled the Header further

--- src/Components/Features.jsx
+++ src/Components/Features.jsx
@@ -1,19 +1,40 @@
-import React from 'react'
+import React from 'react';
+import ImageSlide1 from './../Assets/featuresImageSlide1.png';
+import ImageSlide2 from './../Assets/featuresImageSlide2.jpg';
+import ImageSlide3 from './../Assets/featuresImageSlide3.jpg';
+import ImageSlide4 from './../Assets/featuresImageSlide4.jpg';
+import ImageSlide5 from './../Assets/featuresImageSlide5.jpg';
+import LeftArrow from './../Assets/arrow_circle_left_FILL0_wght400_GRAD0_opsz48 1.svg';
 
 export default function Features() {
+
+  const imageSlides = [
+    { url: ImageSlide1, title: 'Computer' },
+    { url: ImageSlide2, title: 'Computer' },
+    { url: ImageSlide3, title: 'Computer' },
+    { url: ImageSlide4, title: 'Computer' },
+    { url: ImageSlide5, title: 'Computer' },
+  ];
+  console.log(imageSlides);
+
+
   return (
     <section className='features'>
         <div className='features-left'>
-            <img src="" alt="" />
+          <img src={imageSlides[0].url} alt="slides" />
         </div>
 
         <div className='features-right'>
+           <div className='features-text'>
+
            <img src="" alt="" />
            <h2>Responsible Development</h2>
            <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
 
+           </div>
+
            <div className='features-nav'>
-            <div className='arrow-left'><img src="" alt="" /></div>
+            <div className='arrow-left'><img src={LeftArrow} alt="left-arrow" /></div>
 
             <div className='features-pages'>
                 <div className='page-slides slide-1'></div>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useState } from 'react';
 import { Link } from 'react-router-dom'
 
 export default function Header() {
+
+  const [headerIsScrolled, setHeaderIsScrolled] = useState('header-initial');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+      setHeaderIsScrolled('header-scrolled');
+    } else {
+      setHeaderIsScrolled('header-initial');
+    }
+  });
+
   return (
-    <header>
+    <header className={`${headerIsScrolled}`}>
         <h3>Almight <span>Tech</span></h3>
 
         <nav>
