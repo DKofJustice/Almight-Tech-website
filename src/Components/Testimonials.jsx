5fd388d37ce191f461e22079db7e8d49c9dae883
Alex Benta <alexbenta20@gmail.com>
2022-12-10 14:15:21 +0200

    Started on Testimonials

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import React from 'react';
+import TestimonialsBackground from './../Assets/christopher-burns-Kj2SaNHG-hg-unsplash 1.png';
+import TestimonialImage1 from './../Assets/TestimonialImage1.png';
+import TestimonialImage2 from './../Assets/TestimonialImage2.png';
+import TestimonialImage3 from './../Assets/TestimonialImage3.png';
+import UserReview from './../Components/UserReview';
+
 
 export default function Testimonials() {
+
+  const testimonials = [
+    {image: TestimonialImage1},
+    {image: TestimonialImage2},
+    {image: TestimonialImage3}
+  ];
+
   return (
     <section className='testimonials'>
-        <div className='testimonials-background'><img src="" alt="" /></div>
+        <div className='testimonials-background'><img src={TestimonialsBackground} alt="testimonial-background" /></div>
 
         <div className='testimonials-content'>
             <h2>Testimonials</h2>
 
             <div className='testimonials-slider'>
-                
+                {testimonials.map(testimonial => {
+                  return <UserReview image={testimonial.image} />
+                })}
             </div>
         </div>
     </section>
--- src/Components/UserReview.jsx
+++ src/Components/UserReview.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Star from './../Assets/star_FILL0_wght400_GRAD0_opsz48 1.png';
 
-export default function UserReview() {
+export default function UserReview({ image }) {
+
+  let stars = [];
+
+  for(let i = 0; i < 6; i++) {
+    stars.concat(<Star/>);
+    i++;
+  };
+
+  console.log(stars)
+
   return (
     <div className='user-review'>
-        <img src="" alt="" />
+        <img src={image} alt="user-img" />
 
-        <div className='rating'></div>
+        <div className='rating'>{stars}</div>
 
         <p className='user-statement'>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...
 </p>
